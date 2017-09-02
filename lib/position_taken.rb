aacc5e404510b776a09a128760aaf923f8375269
Emily Morgan <emorgan05@gmail.com>
2017-09-02 15:22:20 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,9 +1,5 @@
 def position_taken?(board, index)
-  if board[index] == " "
-    false
-  elsif board[index] == ""
-    false
-  elsif board[index] == nil
+  if board[index] == " " || board[index] == "" || elsif board[index] == nil
     false
   elsif board[index] == "X" || board[index] == "O"
     true
